bda41d8dcd0f06ea1ff043140dd2456118cabba2
The-Graze <me@graze.cc>
2025-11-20 14:15:48 +0000

    a

---
+++ RobloxAudioChecker/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Text.Json;
+using PuppeteerSharp;
+
+namespace RobloxAudioChecker;
+
+internal static partial class RobloxAssetChecker
+{
+    private static async Task Main()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Graze's Audio Checker");
+        Console.WriteLine(new string('-', 50));
+        
+        Console.WriteLine("Downloading Headless Chrome if needed Please wait...");
+        var browserFetcher = new BrowserFetcher();
+        await browserFetcher.DownloadAsync();
+
+        var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+        {
+            Headless = true
+        });
+        var page = await browser.NewPageAsync();
+
+        if (!File.Exists("ids.txt"))
+        {
+            Console.WriteLine("Put a file named 'ids.txt' next to the program");
+            return;
+        }
+        var rawIds = (await File.ReadAllLinesAsync("ids.txt")).ToList();
+        var cleanedIds = rawIds
+            .Select(line => line.Trim())
+            .Where(line => !string.IsNullOrEmpty(line))
+            .Select(line =>
+            {
+                var match = MyRegex().Match(line);
+                return match.Success ? match.Value : null;
+            })
+            .Where(id => id != null)
+            .Distinct()
+            .ToList();
+
+        Console.WriteLine($"Found {cleanedIds.Count}/{rawIds.Count} IDs for checking (duplicates removed)\n");
+
+        var ids = new List<string?>();
+        var total = cleanedIds.Count;
+        
+        var statusLine = Console.CursorTop;
+        var playable = 0;
+        for (var i = 0; i < total; i++)
+        {
+            var id = cleanedIds[i];
+            
+            Console.SetCursorPosition(0, statusLine);
+            var status = "Checking...";
+            var line = $"Checking {id}: {status} ({i + 1}/{total})";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(line.PadRight(Console.WindowWidth));
+
+            var url = $"https://create.roblox.com/store/asset/{id}";
+            await page.GoToAsync(url);
+                await page.WaitForSelectorAsync(
+                    ".MuiButtonBase-root.MuiTab-root.web-blox-css-tss-14jybbz-Typography-body1-tab.MuiTab-textColorInherit.web-blox-css-mui-dsncs0-Typography-button");
+                await Task.Delay(1020);
+            var content = await page.GetContentAsync();
+            
+            if (content.Contains("Audio preview is not available on your browser.") ||
+                content.Contains("MuiTypography-root web-blox-css-tss-a5n33q-Typography-body1-Typography-root-timeStamp MuiTypography-inherit web-blox-css-mui-1de74pe"))
+            {
+                status = "Playable";
+                ids.Add(id);
+                playable++;
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                status = "Moderated";
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            
+            Console.SetCursorPosition(0, statusLine);
+            line = $"Checking {id}: {status} ({i + 1}/{total})";
+            Console.Write(line.PadRight(Console.WindowWidth));
+            
+            Console.ResetColor();
+            
+            await Task.Delay(300);
+        }
+        
+        ids.Add("Sorter by Graze");
+        const string outputFile = "Sorted IDs.txt";
+        var output = ids.Aggregate("Audio Checker by Graze" + "\n", (current, id) => current + (id + "\n"));
+        await File.WriteAllTextAsync(outputFile, output);
+        
+        Console.SetCursorPosition(0, statusLine + 2);
+        Console.WriteLine($"\n {playable}/{total}: \nResults saved to {outputFile}");
+
+        await browser.CloseAsync();
+    }
+
+    [System.Text.RegularExpressions.GeneratedRegex(@"\d+")]
+    private static partial System.Text.RegularExpressions.Regex MyRegex();
+}
